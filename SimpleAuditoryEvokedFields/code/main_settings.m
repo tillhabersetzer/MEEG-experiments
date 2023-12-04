fbd5ab553dd89cdf565c52008f4bbe6c26af7eda
tillhabersetzer <82387317+tillhabersetzer@users.noreply.github.com>
2023-12-04 03:58:50 +0100

    Add gitignore and all the ignored files

--- SimpleAuditoryEvokedFields/code/main_settings.m
+++ SimpleAuditoryEvokedFields/code/main_settings.m
@@ -12,7 +12,7 @@ clear settings
 % FieldTrip path
 settings.path2fieldtrip   = fullfile('C:','Users','tillhabersetzer','Nextcloud','Synchronisation','Projekte','Toolboxen','fieldtrip-20231113');
 % Project path, contains code, rawdata, and derivatives directory
-settings.path2project     = fullfile('C:','Users','tillhabersetzer','Nextcloud','Synchronisation','Projekte','Job','MEG','AEF_experiment');
+settings.path2project     = fullfile('C:','Users','tillhabersetzer','Nextcloud','Synchronisation','Projekte','GitHub','MEEG-experiments','SimpleAuditoryEvokedFields');
 settings.subjects         = {'sub-01','sub-02','sub-03'};
 settings.ref_run_dev2head = 1; % run with reference dev-to-head trafo during coregistration
 % Optional
